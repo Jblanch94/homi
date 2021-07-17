df47b7b691be17a08fb688c70abd90bf8513275c
Johnny Blanchard <johnnyblanchard94@gmail.com>
2021-07-17 10:54:24 -0400

    add types for react-router-dom

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,5 +1,6 @@
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import './index.css';
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
