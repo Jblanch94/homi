8afd84a06372286e8ff7d8652f8f19dea604004a
Johnny Blanchard <johnnyblanchard94@gmail.com>
2021-08-15 10:40:59 -0400

    remove render prop from event form

--- web/src/components/EventForm/EventForm.tsx
+++ web/src/components/EventForm/EventForm.tsx
@@ -50,67 +50,65 @@ const EventForm: FC<{}> = () => {
           validationSchema={AddEventSchema}
           initialValues={initialValues}
           onSubmit={onSubmit}>
-          {(values: FormikValues, setFieldValue: any) => (
-            <Form>
-              <FormHeader name="Add Event" />
-              {isError && (
-                <div style={{ textAlign: "center", color: "red" }}>{error}</div>
-              )}
-              <Grid
-                container
-                spacing={2}
-                className={classes.root}
-                alignItems="flex-end">
-                <Grid item xs={12}>
-                  <TextInput
-                    name="title"
-                    id="title"
-                    type="text"
-                    variant="outlined"
-                    label="Title"
-                    placeholder="Enter the title of the event..."
-                  />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                  <TextInput
-                    name="description"
-                    id="description"
-                    type="text"
-                    variant="outlined"
-                    label="Description"
-                    placeholder="Enter a description for the event..."
-                  />
-                </Grid>
-
-                <Grid item xs={12} md={6}>
-                  <Field name="date" component={DatePickerField} />
-                </Grid>
-
-                <Grid item xs={12} md={6}>
-                  <TextInput
-                    id="startTime"
-                    name="startTime"
-                    variant="outlined"
-                    label="Start Time"
-                    type="time"
-                    InputLabelProps={{ shrink: true }}
-                    inputProps={{ step: 300 }}
-                  />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                  <TextInput
-                    id="endTime"
-                    name="endTime"
-                    variant="outlined"
-                    label="End Time"
-                    type="time"
-                    InputLabelProps={{ shrink: true }}
-                    inputProps={{ step: 300 }}
-                  />
-                </Grid>
+          <Form>
+            <FormHeader name="Add Event" />
+            {isError && (
+              <div style={{ textAlign: "center", color: "red" }}>{error}</div>
+            )}
+            <Grid
+              container
+              spacing={2}
+              className={classes.root}
+              alignItems="flex-end">
+              <Grid item xs={12}>
+                <TextInput
+                  name="title"
+                  id="title"
+                  type="text"
+                  variant="outlined"
+                  label="Title"
+                  placeholder="Enter the title of the event..."
+                />
               </Grid>
-            </Form>
-          )}
+              <Grid item xs={12} md={6}>
+                <TextInput
+                  name="description"
+                  id="description"
+                  type="text"
+                  variant="outlined"
+                  label="Description"
+                  placeholder="Enter a description for the event..."
+                />
+              </Grid>
+
+              <Grid item xs={12} md={6}>
+                <Field name="date" component={DatePickerField} />
+              </Grid>
+
+              <Grid item xs={12} md={6}>
+                <TextInput
+                  id="startTime"
+                  name="startTime"
+                  variant="outlined"
+                  label="Start Time"
+                  type="time"
+                  InputLabelProps={{ shrink: true }}
+                  inputProps={{ step: 300 }}
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <TextInput
+                  id="endTime"
+                  name="endTime"
+                  variant="outlined"
+                  label="End Time"
+                  type="time"
+                  InputLabelProps={{ shrink: true }}
+                  inputProps={{ step: 300 }}
+                />
+              </Grid>
+            </Grid>
+          </Form>
         </Formik>
       </MuiPickersUtilsProvider>
     </>
